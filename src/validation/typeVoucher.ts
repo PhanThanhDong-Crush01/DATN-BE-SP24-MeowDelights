d2f7274db8ca9c4f961233de9913f48822384fb3
LuuThiHuyenTrang <cupitk102@gmail.com>
2024-01-24 21:14:33 +0700

    feat: create nodejs crud type_voucher ref: #2

--- src/validation/typeVoucher.ts
+++ src/validation/typeVoucher.ts
@@ -1,3 +1,5 @@
 import Joi from "joi";
-const typeVoucherSchema = Joi.object({});
+const typeVoucherSchema = Joi.object({
+  name: Joi.string().required().min(6).max(50),
+});
 export default typeVoucherSchema;
