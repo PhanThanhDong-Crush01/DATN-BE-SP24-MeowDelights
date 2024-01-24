65fee76d0d865dbf16ac47a48d28e306d04deb30
PhanThanhDong_Crush <dongptph28020@fpt.edu.vn>
2024-01-24 10:55:28 +0700

    feat: create nodejs add, edit product ref: #1

--- src/routers/product.ts
+++ src/routers/product.ts
@@ -1,12 +1,12 @@
 import express, { Router } from "express";
-import { create, get, getAll, restore, storage } from "../controllers/product";
+import { getAll, restore, storage } from "../controllers/product";
 // import { authorization, } from "../middlewares/authorization";
 // import { authenticate } from "../middlewares/authenticate";
 
 const router: Router = express.Router();
 router.get("/", getAll);
-router.get("/:id", get);
-router.post("/", create);
+// router.get("/products/:id", get);
+// router.post("/products/:id", create);
 router.patch("/restore/:id", restore);
 // router.put("/:id/update", authenticate, authorization, update)
 router.patch("/storage/:id/");
