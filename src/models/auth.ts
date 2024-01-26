22ac92b6c129a3dd51afdc2e8c68a55db6ec1b3f
hoa phung <hoaptph29645@fpt.edu.vn>
2024-01-26 23:57:15 +0700

    feat: Create nodejs add, edit thông tin user ref: #13

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -11,6 +11,7 @@ const authSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+
       unique: true, // Ensure email is unique
     },
     password: {
@@ -18,12 +19,14 @@ const authSchema = new mongoose.Schema(
       // required: true,
       // Hash the password before saving to the database
     },
+
     role: {
       type: String,
       default: "member",
     },
     phone: {
       type: String,
+
       default: "",
     },
     gender: {
