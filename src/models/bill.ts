fe37689845a415666622cc75c926618e3d674f52
hoa phung <hoaptph29645@fpt.edu.vn>
2024-01-26 13:30:04 +0700

    feat: createBill + getAllBill + createBillDetail ref: #8

--- src/models/bill.ts
+++ src/models/bill.ts
@@ -1,6 +1,48 @@
-import mongoose, { Schema } from "mongoose";
-const billSchema: Schema<any> = new mongoose.Schema({
-  timestamps: true,
-  versionKey: false,
-});
-export default mongoose.model("Bill", billSchema);
+import mongoose from "mongoose";
+
+const BillSchema = new mongoose.Schema<any>(
+  {
+    iduser: {
+      type: String,
+      require: true,
+    },
+    money: {
+      type: Number,
+      require: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    adress: {
+      type: String,
+      required: true,
+    },
+    tel: {
+      type: String,
+      required: true,
+    },
+    idvc: {
+      type: String,
+      required: true,
+    },
+    paymentmethods: {
+      type: String,
+      required: true,
+    },
+    paymentstatus: {
+      type: String,
+      required: true,
+    },
+    orderstatus: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+const BillModel = mongoose.model<any>("Bill", BillSchema);
+export default BillModel;
