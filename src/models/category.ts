d9181bb203f1a7d3eb95685a25a13025add67acc
LuuThiHuyenTrang <cupitk102@gmail.com>
2024-02-01 00:30:30 +0700

    update: crud category

--- src/models/category.ts
+++ src/models/category.ts
@@ -5,15 +5,7 @@ const categorySchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      unique: true,
-      defaultValue: "UnCategorized",
     },
-    products: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-      },
-    ],
   },
   {
     versionKey: false,
