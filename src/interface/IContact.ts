014200f07d7e2a994417d7750f6547bf4c1cc064
LuuThiHuyenTrang <cupitk102@gmail.com>
2024-02-21 00:00:14 +0700

    feat: crud contact

---
+++ src/interface/IContact.ts
@@ -0,0 +1,11 @@
+export interface IContact {
+  _id: string;
+  phone: number;
+  email: string;
+  title: string;
+  message: string;
+  idNV: string;
+  statusOrder: boolean;
+  idOrder: string;
+  note: string;
+}
